aaab257f0e1703b1fe3283fa1387ff21521aee06
JFernandoLopez <fernandojlb@outlook.es>
2023-10-28 16:02:16 -0600

    [ADD] firstPathToEvolutionChain

--- PI-Pokemon-main/back/src/index.js
+++ PI-Pokemon-main/back/src/index.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const { router } = require("./routes");
 const app = express()
 
 app.use(express.json())
+
+app.name = 'API';
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use('/pokemons', router)
 
