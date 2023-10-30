cffbcc16094a15e564e03531093ba30c967d01f0
JFernandoLopez <fernandojlb@outlook.es>
2023-10-30 17:23:06 -0600

    [ADD] patchGetByName

--- PI-Pokemon-main/back/src/handlers/getPokemonsByName.js
+++ PI-Pokemon-main/back/src/handlers/getPokemonsByName.js
@@ -2,9 +2,8 @@ const { ctrlGetPokemonsByName } = require('../controllers/ctrlGetPokeByName')
 
 const getPokemonsByName = async (req, res) => {
     const name = req.query.name.toLowerCase();
-    const URL = "https://pokeapi.co/api/v2/pokemon?limit=100";
     try {
-        const pokemon = await ctrlGetPokemonsByName(name, URL)
+        const pokemon = await ctrlGetPokemonsByName(name)
         return res.status(201).json(pokemon)
     } catch (error) {
         return res.status(404).json(error)
